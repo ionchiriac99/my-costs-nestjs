b61fcda01eed0869c9310f31874d199668072811
ionchiriac99 <i.chiriac1999@gmail.com>
2023-07-06 22:50:24 +0300

    change name project

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -3,7 +3,7 @@ import {MongooseModule} from '@nestjs/mongoose';
 import {Token, TokenSchema} from '../auth/token/token.schema';
 import {Account, AccountSchema} from '../auth/account.schema';
 
-const connectionString = 'mongodb://127.0.0.1:27017/my-costs';
+const connectionString = 'mongodb://127.0.0.1:27017/my-transactions';
 
 @Module({
 	imports: [
